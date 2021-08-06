d8df881207dd74c7d45d7d57983c24484233dd0d
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-06 15:17:51 -0400

    First set of tests passed - working on second

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-  render(<CheckoutForm />);
-  screen.getByText("CheckoutForm");
-});
-
-test("form shows success message on submit with form details", () => {
   render(<CheckoutForm />);
 
   //Arrange
@@ -20,17 +15,54 @@ test("form shows success message on submit with form details", () => {
   const cityValue = screen.getByLabelText(/City/i);
   const stateValue = screen.getByLabelText(/State/i);
   const zipValue = screen.getByLabelText(/Zip/i);
-  const checkoutButton = document.querySelector("button");
+  const checkoutButton = document.querySelector("button", {
+    name: /checkout/i,
+  });
 
   //Act
-  userEvent.type(firstNameValue, "Philip");
-  userEvent.type(lastNameValue, "Sherman");
-  userEvent.type(addressValue, "42 Wallaby Way");
-  userEvent.type(cityValue, "Sydney");
-  userEvent.type(stateValue, "Australia");
-  userEvent.type(zipValue, "1291");
-  userEvent.click(checkoutButton);
+  expect(firstNameValue).toBeInTheDocument();
+  expect(lastNameValue).toBeInTheDocument();
+  expect(addressValue).toBeInTheDocument();
+  expect(cityValue).toBeInTheDocument();
+  expect(stateValue).toBeInTheDocument();
+  expect(zipValue).toBeInTheDocument();
+  expect(checkoutButton).toBeInTheDocument();
 
   //Assert
-  const completeMessage = document.querySelector();
+  expect(firstNameValue).toBeVisible();
+  expect(lastNameValue).toBeVisible();
+  expect(addressValue).toBeVisible();
+  expect(cityValue).toBeVisible();
+  expect(stateValue).toBeVisible();
+  expect(zipValue).toBeVisible();
 });
+
+// test("form shows success message on submit with form details", () => {
+//   render(<CheckoutForm />);
+
+//   //Arrange
+//   const firstNameValue = screen.getByLabelText(/First Name/i);
+//   const lastNameValue = screen.getByLabelText(/Last Name/i);
+//   const addressValue = screen.getByLabelText(/Address/i);
+//   const cityValue = screen.getByLabelText(/City/i);
+//   const stateValue = screen.getByLabelText(/State/i);
+//   const zipValue = screen.getByLabelText(/Zip/i);
+//   const checkoutButton = document.querySelector("button", {
+//     name: /checkout/i,
+//   });
+
+//   //Act
+//   userEvent.type(firstNameValue, "Philip");
+//   userEvent.type(lastNameValue, "Sherman");
+//   userEvent.type(addressValue, "42 Wallaby Way");
+//   userEvent.type(cityValue, "Sydney");
+//   userEvent.type(stateValue, "Australia");
+//   userEvent.type(zipValue, "1291");
+//   userEvent.click(checkoutButton);
+
+//   //Assert
+//   const successMessage = screen.getByText(/you have ordered some plants/i);
+
+//   expect(successMessage).toBeTruthy();
+//   expect(successMessage).toBeVisible();
+// });
