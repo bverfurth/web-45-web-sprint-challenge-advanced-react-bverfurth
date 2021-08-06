76fc19c0f2745bcc735c3156f6ea122d0ffc2f5f
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-06 14:00:23 -0400

    adding tests for checkoutform

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -4,6 +4,11 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  screen.getByText("CheckoutForm");
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+});
