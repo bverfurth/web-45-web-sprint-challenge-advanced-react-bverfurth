2ef23f7711710a4e6daa9edb9925100e56964b1c
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-06 14:04:50 -0400

    all arrange set up for form details test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -13,12 +13,12 @@ test("form shows success message on submit with form details", () => {
   render(<CheckoutForm />);
 
   //Arrange
-  const firstNameValue = screen.getByLabelText();
-  const lastNameValue = screen.getByLabelText();
-  const addressValue = screen.getByLabelText();
-  const cityValue = screen.getByLabelText();
-  const stateValue = screen.getByLabelText();
-  const zipValue = screen.getByLabelText();
+  const firstNameValue = screen.getByLabelText(/First Name/i);
+  const lastNameValue = screen.getByLabelText(/Last Name/i);
+  const addressValue = screen.getByLabelText(/Address/i);
+  const cityValue = screen.getByLabelText(/City/i);
+  const stateValue = screen.getByLabelText(/State/i);
+  const zipValue = screen.getByLabelText(/Zip/i);
   const checkoutButton = document.querySelector("button");
 
   //Act
