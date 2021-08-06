d6842db36e6ec91b5ba27844eba3b45f023bf760
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-06 13:45:00 -0400

    setting up custom hook in useform for checkoutform

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,6 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = { initialValue } => {
+    const [value, setValue] = useState(initialValue);
+}
