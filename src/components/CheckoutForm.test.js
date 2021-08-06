87b5e8978064ca3bd11d9fd8d9f16ceb4264e986
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-06 16:09:31 -0400

    working with error on checkoutform test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -7,62 +7,32 @@ import userEvent from "@testing-library/user-event";
 
 test("form header renders", () => {
   render(<CheckoutForm />);
-
-  //Arrange
-  const firstNameValue = screen.getByLabelText(/First Name/i);
-  const lastNameValue = screen.getByLabelText(/Last Name/i);
-  const addressValue = screen.getByLabelText(/Address/i);
-  const cityValue = screen.getByLabelText(/City/i);
-  const stateValue = screen.getByLabelText(/State/i);
-  const zipValue = screen.getByLabelText(/Zip/i);
-  const checkoutButton = document.querySelector("button", {
-    name: /checkout/i,
-  });
-
-  //Act
-  expect(firstNameValue).toBeInTheDocument();
-  expect(lastNameValue).toBeInTheDocument();
-  expect(addressValue).toBeInTheDocument();
-  expect(cityValue).toBeInTheDocument();
-  expect(stateValue).toBeInTheDocument();
-  expect(zipValue).toBeInTheDocument();
-  expect(checkoutButton).toBeInTheDocument();
-
-  //Assert
-  expect(firstNameValue).toBeVisible();
-  expect(lastNameValue).toBeVisible();
-  expect(addressValue).toBeVisible();
-  expect(cityValue).toBeVisible();
-  expect(stateValue).toBeVisible();
-  expect(zipValue).toBeVisible();
+  const header = screen.getByText("Checkout Form");
+  expect(header).toBeInTheDocument;
 });
 
-// test("form shows success message on submit with form details", () => {
-//   render(<CheckoutForm />);
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
 
-//   //Arrange
-//   const firstNameValue = screen.getByLabelText(/First Name/i);
-//   const lastNameValue = screen.getByLabelText(/Last Name/i);
-//   const addressValue = screen.getByLabelText(/Address/i);
-//   const cityValue = screen.getByLabelText(/City/i);
-//   const stateValue = screen.getByLabelText(/State/i);
-//   const zipValue = screen.getByLabelText(/Zip/i);
-//   const checkoutButton = document.querySelector("button", {
-//     name: /checkout/i,
-//   });
+  //Arrange
+  const firstNameValue = screen.getByLabelText("First Name:");
+  const lastNameValue = screen.getByLabelText("Last Name:");
+  const addressValue = screen.getByLabelText("Address:");
+  const cityValue = screen.getByLabelText("City:");
+  const stateValue = screen.getByLabelText("State:");
+  const zipValue = screen.getByLabelText("Zip:");
+  const checkoutButton = document.querySelector("button");
 
-//   //Act
-//   userEvent.type(firstNameValue, "Philip");
-//   userEvent.type(lastNameValue, "Sherman");
-//   userEvent.type(addressValue, "42 Wallaby Way");
-//   userEvent.type(cityValue, "Sydney");
-//   userEvent.type(stateValue, "Australia");
-//   userEvent.type(zipValue, "1291");
-//   userEvent.click(checkoutButton);
+  //Act
+  userEvent.type(firstNameValue, "Philip");
+  userEvent.type(lastNameValue, "Sherman");
+  userEvent.type(addressValue, "42 Wallaby Way");
+  userEvent.type(cityValue, "Sydney");
+  userEvent.type(stateValue, "Australia");
+  userEvent.type(zipValue, "1291");
+  userEvent.click(checkoutButton);
 
-//   //Assert
-//   const successMessage = screen.getByText(/you have ordered some plants/i);
-
-//   expect(successMessage).toBeTruthy();
-//   expect(successMessage).toBeVisible();
-// });
+  //Assert
+  const confirmation = screen.getByTestId("successMessage");
+  expect(confirmation).toBeInTheDocument();
+});
